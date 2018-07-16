dc0a036be2a840e2f1b58ebfd41657f95ef42a03
Villadolid <villadolida@student.douglascollege.ca>
2018-07-16 13:06:09 -0700

    Kicked out of class by kids

--- finalproject.php
+++ finalproject.php
@@ -2,16 +2,46 @@
 
 //Required files
 
+require_once('inc/config.inc.php');
+require_once('inc/object1.inc.php');
+require_once('inc/pdo.inc.php');
+require_once('inc/object1mapper.inc.php');
+require_once('inc/page.inc.php');
+
+$page = new Page();
+
+
+$page->header();
 //Header
 
-//Check GET data to understand what Table we are dealing with
+$obj;
+
+//Check GET data to understand what Table we are dealing with and create an ObjectMapper based on that
+if(false){
+
+}elseif(false){
+
+}else{
+    //Create a default table
+    $objMapper = new object1Mapper("Cars");
+}
+
 
 //Check POST data to see if we should delete/edit. If yes, unset GET variables.
 
 //Check other GET data to see if we should display edit form or add a new object 
 
+
+$page->tableChooser();
+
+$page->addForm();
+
+$page->diplayData();
+
 //Display the data
 
 //Footer
+$page->footer();
+
 
 ?>
