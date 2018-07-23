20407e86a33880a6df3849825a31c5319d7a19cc
Ladoli <villadolidangeloryan@gmail.com>
2018-07-23 06:07:49 -0700

    Temporary Object Files

--- finalproject.php
+++ finalproject.php
@@ -3,9 +3,9 @@
 //Required files
 
 require_once('inc/config.inc.php');
-require_once('inc/object1.inc.php');
+require_once('inc/owner.class.php');
 require_once('inc/pdo.inc.php');
-require_once('inc/object1mapper.inc.php');
+require_once('inc/ownerMapper.class.php');
 require_once('inc/page.inc.php');
 
 $page = new Page();
@@ -27,90 +27,90 @@ if(isset($_GET['tables'])){
 if($tableName === "Transportation Type"){
     $objMapper = new object1Mapper("Transportation Type");
     if (empty($_POST))  {
-    
+
     } else {
-    
+
         //Verify the post data
         if (   empty($_POST['type'])
             || empty($_POST['name'])
             || empty($_POST['description'])
             || empty($_POST['wheels']) )  {
-    
+
             //Display an alert
             echo '<DIV CLASS="alert alert-danger">You have not entered the appropriate details.<br/>
             Please go back and try again.</DIV> ';
-            
+
             exit;
         } else {
-    
+
                 //We have the right data, lets go ahead and add the customer via the customer mapper
                 unset($_GET['id']);
                 unset($_GET['action']);
                 $newid = $objMapper->create($_POST);
-                
+
                 //Display a message to the user that the customer has been created
                 echo '<DIV CLASS="alert alert-success">New '.$tableName.' '.$newid.' has been created!<BR></DIV>';
         }
-    
+
     }
 }elseif($tableName === "Owner"){
     $objMapper = new object1Mapper("Owner");
     if (empty($_POST))  {
-    
+
     } else {
-    
+
         //Verify the post data
         if (   empty($_POST['name'])
             || empty($_POST['city'])
             || empty($_POST['gender'])
             || empty($_POST['family']) )  {
-    
+
             //Display an alert
             echo '<DIV CLASS="alert alert-danger">You have not entered the appropriate details.<br/>
             Please go back and try again.</DIV> ';
-            
+
             exit;
         } else {
-    
+
                 //We have the right data, lets go ahead and add the customer via the customer mapper
                 unset($_GET['id']);
                 unset($_GET['action']);
                 $newid = $objMapper->create($_POST);
-                
+
                 //Display a message to the user that the customer has been created
                 echo '<DIV CLASS="alert alert-success">New '.$tableName.' '.$newid.' has been created!<BR></DIV>';
         }
-    
+
     }
 }else{
     //Create a default table
     $objMapper = new object1Mapper("Vehicle");
     if (empty($_POST))  {
-    
+
     } else {
-    
+
         //Verify the post data
         if (   empty($_POST['makeModel'])
             || empty($_POST['color'])
             || empty($_POST['owner'])
             || empty($_POST['type']) )  {
-    
+
             //Display an alert
             echo '<DIV CLASS="alert alert-danger">You have not entered the appropriate details.<br/>
             Please go back and try again.</DIV> ';
-            
+
             exit;
         } else {
-    
+
                 //We have the right data, lets go ahead and add the customer via the customer mapper
                 unset($_GET['id']);
                 unset($_GET['action']);
                 $newid = $objMapper->create($_POST);
-                
+
                 //Display a message to the user that the customer has been created
                 echo '<DIV CLASS="alert alert-success">New '.$tableName.' '.$newid.' has been created!<BR></DIV>';
         }
-    
+
     }
 }
 
@@ -119,7 +119,7 @@ if (isset($_GET['action']) && $_GET['action'] == "delete" && isset($_GET['id']))
     //Delete the object
     $results = $objMapper->delete($_GET['id']);
     echo '<DIV CLASS="alert alert-success">Customer '.$results.' has been deleted.</DIV>';
-} 
+}
 
 
 
